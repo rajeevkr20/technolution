424f69b9b9fda8d55553735beba8fa227c0eff61
Rajeev <rajeevkr@rssoftware.co.in>
2017-02-20 21:16:25 +0530

    upd

--- src/main/java/com/rajeev/test/controller/TecholutionController.java
+++ src/main/java/com/rajeev/test/controller/TecholutionController.java
@@ -22,44 +22,59 @@ import com.rajeev.test.util.SatisfactionTime;
 @RequestMapping("/techolution")
 public class TecholutionController {
 	private Logger log = Logger.getLogger(TecholutionController.class);
-	private int noOfComperasion = 0;
 	
 	private int haveTime;
 	private int noOfItem;
-	private int satisfactionsc =0;
-	
-	@RequestMapping( value = "/findSatisfaction")
-	public Object findSatisfaction( HttpServletRequest request,Model model) throws Exception
-	{
-		noOfComperasion = 0;
-	
+	private int satisfactionsc = 0;
+
+	/**
+	 * @param request
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	/**
+	 * @param request
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/findSatisfaction")
+	public Object findSatisfaction(HttpServletRequest request, Model model)
+			throws Exception {
+		
+
 		List<SatisfactionTime> satsfactionTimeList = readInputFile();
 		Collections.sort(satsfactionTimeList, new TimeTakenComperator());
-		int satisfactionScore = getMaxSatisfaction(satsfactionTimeList );
+		int satisfactionScore = getMaxSatisfaction(satsfactionTimeList);
 		satisfactionsc = satisfactionScore;
-		
+
 		Collections.sort(satsfactionTimeList, new SatisfactionComperator());
-		satisfactionScore = getMaxSatisfaction(satsfactionTimeList );
-		
-		if(satisfactionsc < satisfactionScore){
+		satisfactionScore = getMaxSatisfaction(satsfactionTimeList);
+
+		if (satisfactionsc < satisfactionScore) {
 			satisfactionsc = satisfactionScore;
 		}
 		return satisfactionsc;
 	}
-private int getMaxSatisfaction(List<SatisfactionTime> satisfactionTimeList) {
-		
-		
+
+	/**
+	 * @param satisfactionTimeList
+	 * @return
+	 */
+	private int getMaxSatisfaction(List<SatisfactionTime> satisfactionTimeList) {
 
 		int satisfactionScore = 0;
-		for(int i = 0; i<satisfactionTimeList.size(); i++){
-			
-			List<SatisfactionTime> listSliceNo = satisfactionTimeList.subList(i, satisfactionTimeList.size());
+		for (int i = 0; i < satisfactionTimeList.size(); i++) {
+
+			List<SatisfactionTime> listSliceNo = satisfactionTimeList.subList(
+					i, satisfactionTimeList.size());
 			int satisfaction = getSatisfaction(listSliceNo);
 
-			if(satisfactionScore < satisfaction){
+			if (satisfactionScore < satisfaction) {
 				satisfactionScore = satisfaction;
 			}
-		
+
 		}
 		return satisfactionScore;
 	}
@@ -71,74 +86,72 @@ private int getMaxSatisfaction(List<SatisfactionTime> satisfactionTimeList) {
 	 * @return current satisfaction scores
 	 */
 	private int getSatisfaction(List<SatisfactionTime> satisfactionTimeList) {
-		
+
 		int score = 0;
 		int time = 0;
-		int noOfItemTry =0;
-		for(SatisfactionTime st : satisfactionTimeList){
-			score = score+ st.getSatisfactionAmount();
+		int noOfItemTry = 0;
+		for (SatisfactionTime st : satisfactionTimeList) {
+			score = score + st.getSatisfactionAmount();
 			time = time + st.getTimeTaken();
 			noOfItemTry++;
-			if(time < haveTime && noOfItemTry <= noOfItem )
+			if (time < haveTime && noOfItemTry <= noOfItem)
 				continue;
 			else {
-				
+
 				break;
 			}
 		}
-		
-		
+
 		return score;
-		
+
 	}
-	
-	
-	
+
 	/**
 	 * @return
 	 */
-	private List<SatisfactionTime> readInputFile(){
+	private List<SatisfactionTime> readInputFile() {
 		BufferedReader br = null;
 		List<SatisfactionTime> satsfactionTimeList = new ArrayList<SatisfactionTime>();
-		
+
 		try {
 			br = new BufferedReader(new FileReader("data.txt"));
-		
-		    String line = br.readLine();
-		    int i=0;
-		    int j= 0;
-		    while (line != null) {
-		    	
-		       String [] strLine = line.split(" ");
-		       
-		       if(j==0){
-		    	   haveTime = Integer.parseInt(strLine[0]);
-		    	   noOfItem = Integer.parseInt(strLine[1]);
-		       }
-		       else {
-		    	   SatisfactionTime st = new SatisfactionTime();
-			       st.setSatisfactionAmount(strLine[0] != null ? Integer.parseInt(strLine[0]) : null);
-			       st.setTimeTaken(strLine[1] != null ? Integer.parseInt(strLine[1]) : null);
-			       st.setIndex(i++);
-		    	   satsfactionTimeList.add(st);
-		       }//System.out.println(st.toString());
-		       line = br.readLine();
-		       j++;
-		    }
-		   
+
+			String line = br.readLine();
+			int i = 0;
+			int j = 0;
+			while (line != null) {
+
+				String[] strLine = line.split(" ");
+
+				if (j == 0) {
+					haveTime = Integer.parseInt(strLine[0]);
+					noOfItem = Integer.parseInt(strLine[1]);
+				} else {
+					SatisfactionTime st = new SatisfactionTime();
+					st.setSatisfactionAmount(strLine[0] != null ? Integer
+							.parseInt(strLine[0]) : null);
+					st.setTimeTaken(strLine[1] != null ? Integer
+							.parseInt(strLine[1]) : null);
+					st.setIndex(i++);
+					satsfactionTimeList.add(st);
+				}// System.out.println(st.toString());
+				line = br.readLine();
+				j++;
+			}
+
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
 		} finally {
-		    try {
+			try {
 				br.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 		return satsfactionTimeList;
 	}
-	
+
 }
