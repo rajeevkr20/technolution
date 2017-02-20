75537612541c742ba18ef475a329111f40d95105
Rajeev <rajeevkr@rssoftware.co.in>
2017-02-20 21:12:08 +0530

    cv

---
+++ src/main/java/com/rajeev/test/TechnolutionApplication.java
@@ -0,0 +1,12 @@
+package com.rajeev.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnolutionApplication.class, args);
+	}
+}
